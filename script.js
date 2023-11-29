55192e3b5454eff1734ebb266860e44e2fbe92b3
Fabian Tovar <thefabiantovar@gmail.com>
2023-11-28 21:51:15 -0800

    added a bit more css for layout. Added a toggle button to display/hide grid that will be implemented later on

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function darkMode() {
   const darkBtn = document.body;
   darkBtn.classList.toggle("dark-mode");
 }
+
+// hide grid btn
+function hideGrid() {
+  const grid = document.getElementById("percentages-grid");
+  if (grid.style.display === "none") {
+    grid.style.display = "block";
+  } else {
+    grid.style.display = "none";
+  }
+}
