37e571dc7d1bf7c879fdc2d4cfbfd744a07ed610
Fabian Tovar <thefabiantovar@gmail.com>
2023-12-01 21:37:12 -0800

    created numeric grid of buttons. Created functionality to print percentage numbers when buttons are clicked

--- script.js
+++ script.js
@@ -15,3 +15,30 @@ function hideGrid() {
     grid.style.display = "none";
   }
 }
+
+//target button value when clicked
+const gridBtns = document.querySelectorAll(".grid-btn");
+
+gridBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    //grabs current value of button
+    const value = this.value;
+    addToList(value);
+    console.log(value);
+  });
+});
+
+//add clicked percentages to list
+function addToList(percentageVal) {
+  const selectedPercentagesList = document.querySelector(
+    ".percentage-list-info"
+  );
+
+  const newPercentage = document.createElement("li");
+  newPercentage.style.backgroundColor = "lightblue";
+  newPercentage.style.border = "solid red";
+  newPercentage.textContent = percentageVal; //percentageVal
+  selectedPercentagesList.appendChild(newPercentage);
+}
+
+//Reset Button to clear list
