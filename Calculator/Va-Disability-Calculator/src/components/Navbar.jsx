d0615af17084ce02f8ea7bdfbbe183f7f0285dff
Fabian Tovar <thefabiantovar@gmail.com>
2024-09-07 14:26:01 -0700

    added react framework. Transferring data to react structure. Created navbar component

---
+++ Calculator/Va-Disability-Calculator/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <div>Navbar</div>
+      <button>Dark Mode</button>
+      <button>Reset</button>
+    </>
+  );
+}
